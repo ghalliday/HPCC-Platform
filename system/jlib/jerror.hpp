c76db4e7a43ebfe63959064825cad4507697bae8
Gavin Halliday <gavin.halliday@lexisnexis.com>
2026-04-14 08:27:40 +0100

    jlib: use standard error codes for jfile instead of magic numbers

--- system/jlib/jerror.hpp
+++ system/jlib/jerror.hpp
@@ -59,6 +59,45 @@
 #define JLIBERR_SocketWaitMultipleError        6031
 #define JLIBERR_BufferedSocketFromNull         6032
 
+#define JLIBERR_FileCopyTargetCreateFailed          6033
+#define JLIBERR_FileCopySourceNotFound              6034
+#define JLIBERR_FileRenameSourceNotFound            6035
+#define JLIBERR_FileRenameSourceInvalid             6036
+#define JLIBERR_FileRenameSourceReadOnly            6037
+#define JLIBERR_FileRenameTargetCreateFailed        6038
+#define JLIBERR_FileRenameTargetExists              6039
+#define JLIBERR_ExtractBlobElementsFileNotFound     6040
+#define JLIBERR_FileLockInvalidParameter            6041
+#define JLIBERR_MemoryMapRemoteFileError            6042
+#define JLIBERR_MemoryBufferReadBeyondEnd           6043
+#define JLIBERR_MemoryBufferWriteBeyondEndUnimplemented  6044
+#define JLIBERR_MemoryBufferSetSizeBeyondEndUnimplemented 6045
+#define JLIBERR_SequentialFileOutOfSequence         6046
+#define JLIBERR_FileStreamSeekNotSeekable           6047
+#define JLIBERR_CreateIFileCannotResolve            6048
+#define JLIBERR_CreateIFileCannotAttach             6049
+#define JLIBERR_TouchFileFailedToCreate             6050
+#define JLIBERR_CreateHardLinkFailed                6051
+#define JLIBERR_CreateLogAliasFailed                6052
+#define JLIBERR_BadlyFormattedFileEntry             6053
+#define JLIBERR_ComponentFileIPMismatch             6054
+#define JLIBERR_MakeAbsolutePathResolveFailed       6055
+#define JLIBERR_FailedToOpenPartFileAnyLocation     6056
+#define JLIBERR_InputStreamReadPastEnd              6057
+#define JLIBERR_StreamFileTooBigToMap               6058
+#define JLIBERR_MappedStreamReadPastEnd             6059
+#define JLIBERR_MappedStreamSkipPastEnd             6060
+#define JLIBERR_BufferStreamReadPastEnd             6061
+#define JLIBERR_BufferStreamSkipPastEnd             6062
+#define JLIBERR_MemoryMappedFileOutsideMap          6063
+#define JLIBERR_MemoryMappedFileReinitTooBig        6064
+#define JLIBERR_CachedFileIONotSupported            6065
+#define JLIBERR_CachedFileIOOpenFailed              6066
+#define JLIBERR_InvalidIFileInputSortedDirIterator  6067
+#define JLIBERR_InvalidDirNameInputSortedDirIterator 6068
+#define JLIBERR_FileEventWatcherAddInvalidEmpty     6069
+#define JLIBERR_ReentrantCallCBlockedFileIORead     6070
+
 //---- Text for all errors (make it easy to internationalise) ---------------------------
 
 #define JLIBERR_BadlyFormedDateTime_Text        "Badly formatted date/time '%s'"
